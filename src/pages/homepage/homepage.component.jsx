d7b9eec885c6235dd28f6c4d3756197d8394665e
unknown <3dgeostudio@gmail.com>
2021-09-06 22:52:42 +0200

    updating our app with a shop page, collection preview component and collection item component

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -4,12 +4,10 @@ import Directory from '../../components/directory/directory.component';
 
 import './homepage.styles.scss';
 
-const HomePage = (prom) => (
-  console.log(prom),
+const HomePage = () => (
   <div className='homepage'>
     <Directory />
   </div>
-
 );
 
 export default HomePage;
